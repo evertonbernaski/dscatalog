b8bf3679e6f733577e923deabd6a833165c279bd
everton.bernaski <everton.bernaski@objective.com.br>
2022-05-29 19:50:52 -0300

    Update

--- backend/src/main/java/com/dscatalog/dscatalog/services/exceptions/ResourceNotFoundException.java
+++ backend/src/main/java/com/dscatalog/dscatalog/services/exceptions/ResourceNotFoundException.java
@@ -1,9 +1,9 @@
 package com.dscatalog.dscatalog.services.exceptions;
 
-public class EntityNotFoundException extends RuntimeException {
+public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 4333061262017367368L;
 
-    public EntityNotFoundException(String msg) {
+    public ResourceNotFoundException(String msg) {
         super(msg);
     }
 }
