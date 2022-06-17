8ea1be50c87647e9119ab7e3ee753b72e6c8ea44
Oliveira Bernaski <enoi@gft.com>
2022-06-17 15:20:24 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/dscatalog/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.dscatalog.dscatalog.repositories;
+
+import com.dscatalog.dscatalog.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
